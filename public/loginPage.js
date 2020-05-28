6da5787f643750eb37869db39a8d2c44522b306d
Marina Muratova <frau.muratova@gmail.com>
2020-05-28 19:36:16 +0300

    Initial commit

---
+++ public/loginPage.js
@@ -0,0 +1,8 @@
+'use strict' //Подключите строгий режим выполнения кода
+let userForm = new UserForm(); //Создайте объект класса UserForm
+
+userForm.loginFormCallback = function(data){ //Присвойте свойству loginFormCallback созданного объекта значение функции, которая в качестве аргумента принимает объект data
+
+}
+
+
