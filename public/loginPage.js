7cdcc6868a34daa36909352d9f73617c2345021b
Marina Muratova <frau.muratova@gmail.com>
2020-05-29 18:24:16 +0300

    loginPage done

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,22 +1,21 @@
-'use strict' //Подключите строгий режим выполнения кода
-let userForm = new UserForm(); //Создайте объект класса UserForm
-const loginForm = _getData(form);
-userForm.loginFormCallback = function(data){ //Присвойте свойству loginFormCallback созданного объекта значение функции, которая в качестве аргумента принимает объект data
-//Функция должна выполнять запрос на сервер для попытке авторизации пользователя (авторизацию пользователя выполняйте с помощью ApiConnector.login)
-loginFormAction(data);//Передайте в запрос авторизации функцию, которая будет выполняться при попытке авторизации
-    if (request){ //В случае успеха запроса обновите страницу 
-	   location.reload();
+'use strict' 
+let userForm = new UserForm(); 
+userForm.loginFormCallback = function(data){ 
+ ApiConnector.login(data, (response) => {
+    if (response.success){ 
+       location.reload();
     } else{
-	   return setLoginErrorMessage(message);//В случае провала запроса выведите ошибку в окно для ошибок
+       return setLoginErrorMessage(message);
     }
+  });   
 }
 
 userForm.registerFormCallback = function(data){
-	registerFormAction(data);//Передайте запрос на регистрацию.
-//Напишите колбек, который будет выполняться после запроса.
-  if (request){ //В случае успеха запроса обновите страницу 
-	   location.reload();
+ ApiConnector.login(data, (response) => {
+    if (response.success){ 
+       location.reload();
     } else{
-	   return setRegisterErrorMessage(message);//В случае провала запроса выведите ошибку в окно для ошибок
+       return setLoginErrorMessage(message);
     }
+  });   
 }
